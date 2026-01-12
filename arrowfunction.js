1b3163ac5b662da8805fa7f10cb34ee6e78c55ce
amitsavani-45 <apsavani4518@gmail.com>
2026-01-12 14:39:13 +0530

    Arrow function in js

---
+++ arrowfunction.js
@@ -0,0 +1,22 @@
+import promptsync from 'prompt-sync';
+const prompt=promptsync()
+
+
+
+const greet= () => "hello javascript";
+console.log(greet());
+
+//double to number 
+
+const double= num => num*2;
+console.log(double(5));
+
+//square of number 
+
+const sq= a=> a*a;
+console.log(sq(15));
+
+//Cube of number
+
+const cube= (num)=>num**3;
+console.log(cube(3));
