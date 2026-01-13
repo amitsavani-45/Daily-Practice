c7d7a9202598173487ba4f2d20da9fb8d3b9522a
amitsavani-45 <apsavani4518@gmail.com>
2026-01-13 13:22:39 +0530

    Array numberic sort

---
+++ arraynumbersort.js
@@ -0,0 +1,10 @@
+// Numeric Sort
+// Random Sort
+// Math.min()
+// Math.max()
+// Home made Min()
+// Home made Max()
+
+//  Numeric Sort
+// By default, sort() converts numbers to strings. Use a compare function for proper numeric sorting.
+
