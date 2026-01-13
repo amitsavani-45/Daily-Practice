be50f1c9cf72a79dfbe41e3ce00ce63eb86e58e6
amitsavani-45 <apsavani4518@gmail.com>
2026-01-13 12:55:27 +0530

    Array sort in js

--- arraysort.js
+++ arraysort.js
@@ -5,3 +5,21 @@
 // Array toReversed()
 // Sorting Objects
 
+// Array.sort() - Alphabetic Sort
+// Sorts array elements alphabetically (modifies original array)
+
+const fruits=['banana','apple','mango','pineapple','orange'];
+fruits.sort();
+console.log(fruits);
+
+
+// Array.reverse()
+// Reverses the order of elements (modifies original array)
+
+const numbers = [1, 2, 3, 4, 5];
+numbers.reverse();
+console.log(numbers);  
+
+const letters = ['a', 'b', 'c'];
+letters.reverse();
+console.log(letters); 
