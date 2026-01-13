c696ee3bb7ffaecddd1de944f1a2670a3565efe3
amitsavani-45 <apsavani4518@gmail.com>
2026-01-13 13:16:05 +0530

    Array Sort in JS

--- arraysort.js
+++ arraysort.js
@@ -23,3 +23,38 @@ console.log(numbers);
 const letters = ['a', 'b', 'c'];
 letters.reverse();
 console.log(letters); 
+
+
+// Array.toSorted()
+// Returns a new sorted array (original stays unchanged).
+const fruit = ['banana', 'apple', 'orange'];
+const sorted = fruit.toSorted();
+
+console.log(sorted);  
+console.log(fruit);  
+
+// Array.toReversed()
+// Returns a new reversed array (original stays unchanged).
+
+const nu = [1, 2, 3, 4, 5];
+const reversed = nu.toReversed();
+
+console.log(reversed);  
+console.log(nu);   
+
+//  Sorting Objects
+// Sort arrays of objects using a compare function.
+
+
+const products = [
+  { name: 'Phone', price: 500 },
+  { name: 'Laptop', price: 1000 },
+  { name: 'Mouse', price: 20 }
+];
+
+
+products.sort((a, b) => a.price - b.price);
+console.log(products);
+
+products.sort((a, b) => b.price - a.price);
+console.log(products);
