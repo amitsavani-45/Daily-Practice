00f996b8f3287bbf1e1060d9327fbe7989644ab4
amitsavani-45 <apsavani4518@gmail.com>
2026-01-14 22:41:23 +0530

    dowhile in js

--- dowhile.js
+++ dowhile.js
@@ -17,3 +17,19 @@ do{
     j--;
 }
 while(j>=0)
+
+//print even numbers from 0 to 20
+
+let k=0;
+do{
+    console.log(k);
+    k+=2;
+}
+while(k<=20)
+//print odd numbers from 1 to 19
+
+let m=1; 
+do{
+    console.log(m);
+    m+=2;
+}   
