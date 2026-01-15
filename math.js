7c813f8758513d662b3a593aaf994045362033b4
amitsavani-45 <apsavani4518@gmail.com>
2026-01-15 16:21:46 +0530

    Math in js

---
+++ math.js
@@ -0,0 +1,30 @@
+let x=7.4;
+	
+console.log(Math.ceil(x));
+console.log(Math.floor(x));	
+console.log(Math.trunc(x));
+console.log(Math.round(x));
+console.log(Math.E);
+       
+console.log(Math.PI);       
+console.log(Math.SQRT2);    
+console.log(Math.SQRT1_2);  
+console.log(Math.LN2);      
+console.log(Math.LN10);    
+console.log(Math.LOG2E);    
+console.log(Math.LOG10E);
+
+
+console.log(Math.sign(-11));
+
+console.log(Math.pow(8, 2));
+
+console.log(Math.sqrt(64));
+console.log(Math.abs(-4.7));
+console.log(Math.min(0, 150, 30, 20, -8, -200));
+console.log(Math.max(0, 150, 30, 20, -8, -200));
+console.log(Math.random());
+console.log(Math.log(2));
+console.log(Math.log10(1000));
+
+
