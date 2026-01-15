27a16c375c4d4c68a729098635f30a9de06cec13
amitsavani-45 <apsavani4518@gmail.com>
2026-01-15 09:50:33 +0530

    set in js

--- set.js
+++ set.js
@@ -29,3 +29,14 @@ fruits.forEach(value =>
     console.log(value.toUpperCase())
 });
 
+// size - Get number of elements
+
+console.log(fruits.size);
+console.log(letter.size);
+
+// values() - Get all the values
+// This gives you each item in the Set, one by one.
+
+for (const fruit of fruits.values()) {
+  console.log(fruit);
+}
