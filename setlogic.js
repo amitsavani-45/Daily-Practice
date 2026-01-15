ed79391fbea9581073f5c3453bfbc3c17fc8b729
amitsavani-45 <apsavani4518@gmail.com>
2026-01-15 11:06:09 +0530

    setlogic

---
+++ setlogic.js
@@ -0,0 +1,7 @@
+// . union() - Combine two Sets (all items from both)
+// Think: "Everything from Set A OR Set B"
+const setA = new Set([1, 2, 3]);
+const setB = new Set([3, 4, 5]);
+
+const result = setA.union(setB);
+console.log(result); 
