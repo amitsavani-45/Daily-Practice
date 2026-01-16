99891d1f363121fb7883f777f05cbc6d4f927f1a
amitsavani-45 <apsavani4518@gmail.com>
2026-01-16 14:54:05 +0530

    class in js

--- class.js
+++ class.js
@@ -11,3 +11,24 @@ class Car {
 
 let car1 = new Car("Toyota", "Red");
 car1.drive();
+
+
+class player{
+    constructor(player,team)
+    {
+        this.player=player;
+        this.team=team;
+    }
+
+
+play() 
+  {console.log(this.player +" "+"is playing for " +" " +this.team);
+        }
+}
+let player1=new player("virat Kohli","RCB");
+let player2=new player("Rohit Sharma","MI");
+
+
+player1.play();
+player2.play();
+    
