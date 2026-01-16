4beb8722e2eaab88a4e827964b761127c1bd8da2
amitsavani-45 <apsavani4518@gmail.com>
2026-01-16 12:12:00 +0530

    functioncall

--- functioncall.js
+++ functioncall.js
@@ -34,7 +34,66 @@ const calculator = {
 };
 
 
-const numbers = [10, 20, 30];
+const numbers1 = [10, 20, 30];
 
-let result2 = calculator.sum.apply(null, numbers);
+let result2 = calculator.sum.apply(null, numbers1);
 console.log(result2);  
+
+
+let numbers2 = [5, 6, 2, 3, 7];
+
+let max = Math.max.apply(null, numbers2);
+console.log(max); 
+
+let min = Math.min.apply(null, numbers2);
+console.log(min); 
+
+
+
+
+//bind() in function
+
+const person4 = {
+   
+    fullName: function() {
+        return this.firstName + " " + this.lastName;
+    }
+};
+
+const user = {
+    firstName: "Amit",
+    lastName: "Savani"
+};
+
+
+let result1 = person4.fullName.call(user);
+console.log(result1);  
+
+
+let getUserName = person4.fullName.bind(user);
+console.log(getUserName);  
+
+let res = getUserName();
+console.log(res);  
+
+
+
+const player={
+    name: function(){
+        return this.name+ " "+ this.teamname;
+    }
+};
+
+const team={
+    name: "virat",
+    teamname: "RCB"
+};
+
+let give=player.name.call(team);
+console.log(give);
+
+let giveteam=player.name.bind(team);
+console.log(giveteam);
+
+let teamname=giveteam();
+console.log(teamname);
