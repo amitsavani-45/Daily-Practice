6e6744bfae27ba7cf1704c166a087e6d135ef894
amitsavani-45 <apsavani4518@gmail.com>
2026-01-16 16:42:16 +0530

    Abstraction

---
+++ Abstraction.js
@@ -0,0 +1,10 @@
+/*Abstraction
+
+Definition: Abstraction is the concept of showing only essential features of an object while hiding the unnecessary details.
+
+Key points:
+
+Focuses on what an object does, not how it does it.
+
+Achieved using abstract classes or interfaces in some languages.
+*/
