838ef1b7c58b44767ecd4cd632bb0667d1c2c210
amitsavani-45 <apsavani4518@gmail.com>
2026-01-16 16:45:12 +0530

    Polymorphism

---
+++ Polymorphism.js
@@ -0,0 +1,10 @@
+/*Polymorphism
+
+Definition: Polymorphism means “many forms”—the ability of an object to take multiple forms.
+
+Key points:
+
+Can be achieved through method overloading (same method name, different parameters) and method overriding (child class changes parent’s method behavior).
+
+Makes code more flexible and extensible.
+*/
