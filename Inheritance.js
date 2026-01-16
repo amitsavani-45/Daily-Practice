6afc866750e6280a538ac2f284e49133adc4b002
amitsavani-45 <apsavani4518@gmail.com>
2026-01-16 16:43:40 +0530

    Inheritance

---
+++ Inheritance.js
@@ -0,0 +1,10 @@
+/*Inheritance
+
+Definition: Inheritance allows a class (child/subclass) to acquire properties and behaviors of another class (parent/superclass).
+
+Key points:
+
+Promotes code reuse.
+
+Supports a hierarchical relationship.
+*/
