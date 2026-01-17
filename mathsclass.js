7f5702749b3724eee40553a677cdfe5096e21bd2
amitsavani-45 <apsavani4518@gmail.com>
2026-01-17 11:39:20 +0530

    maths class

--- mathsclass.js
+++ mathsclass.js
@@ -14,7 +14,7 @@ sub(a,b){
     return a-b;
 }
 
-mul(a,b){
+mulsk(a,b){
     return a*b;
 }
 
@@ -30,6 +30,29 @@ console.log("addition is"+" "+cal.add(20,30));
 
 console.log("subcration is"+" "+cal.sub(45,23));
 
-console.log("multiplication is"+" "+cal.mul(9,5));
+console.log("multiplication is"+" "+cal.mulsk(9,5));
 
 console.log("division is"+" "+cal.div(45,9));
+
+class calculation{
+
+    constructor(){
+        this.mathsop=new maths();
+    }
+
+
+
+
+table(number,range=10){
+    console.log("\n multiplication table for "+ number + ":");
+
+    for(let i=1; i<=range; i++)
+    {
+        let result=this.mathsop.mulsk(number,i);
+        console.log(number+" "+"X"+" "+i+" "+"="+result);
+    }
+}
+}
+
+const tableCalc = new calculation();  +" "
+tableCalc.table(2);
