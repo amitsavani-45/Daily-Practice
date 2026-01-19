02954e9a2d88ef1d0c93d25a98ab897c7fc14e21
amitsavani-45 <apsavani4518@gmail.com>
2026-01-19 12:56:25 +0530

    whileloop example

--- whileloop.js
+++ whileloop.js
@@ -18,16 +18,49 @@ while(a>=0)
     console.log(a);
     a--;
 }
-*/
 
+*/
 //mysirg in N time print
 import promptsync from 'prompt-sync';
 const prompt=promptsync()
-let write="mysirg";
+
+/*
 let n=prompt("Enter the value ");
 let i=0;
 while(i<=n)
 {
-    console.log(write);
+    console.log("mysirg");
     i++;
 }
+
+let n=prompt("Enter the value ");
+let i=1;
+while(i<=n){
+    console.log("the natural number is",i);
+    i++
+}
+   
+   
+let n=prompt("Enter the value ");
+let i=n;
+while(i>=1){
+      console.log(i);
+       i--;
+} 
+
+let n=prompt("Enter the value ");
+let i=1;
+while(i<=n){
+    console.log(i);
+    i+=2;
+}
+
+
+// n give value even when need reverse even natural number and n give value odd when need reverse odd natural number
+
+let n=prompt("Enter the value ");
+let i=n;
+while(i>=2){
+    console.log(i);
+    i-=2;
+}   */
