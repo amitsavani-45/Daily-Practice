4ebfec4537a5aa59342af9e6b7b42290c6f4fcc6
amitsavani-45 <apsavani4518@gmail.com>
2026-01-19 12:33:55 +0530

    whileloop example

--- whileloop.js
+++ whileloop.js
@@ -1,3 +1,4 @@
+/*
 // print 0 to 5
 
 let i=0;
@@ -17,4 +18,16 @@ while(a>=0)
     console.log(a);
     a--;
 }
+*/
 
+//mysirg in N time print
+import promptsync from 'prompt-sync';
+const prompt=promptsync()
+let write="mysirg";
+let n=prompt("Enter the value ");
+let i=0;
+while(i<=n)
+{
+    console.log(write);
+    i++;
+}
