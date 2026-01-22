086e9046a03db14acb2e3715f77a251562c5a008
amitsavani-45 <apsavani4518@gmail.com>
2026-01-22 22:11:21 +0530

    inheritance

--- Inheritance.js
+++ Inheritance.js
@@ -8,3 +8,28 @@ Promotes code reuse.
 
 Supports a hierarchical relationship.
 */
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+
+  introduce() {
+    console.log("My name is " + this.name);
+  }
+}
+
+class Student extends Person {
+  constructor(name, rollNo) {
+    super(name);
+    this.rollNo = rollNo;
+  }
+
+  study() {
+    console.log(this.name + " is studying");
+  }
+}
+
+let s1 = new Student("Amit", 101);
+s1.introduce();
+s1.study();
