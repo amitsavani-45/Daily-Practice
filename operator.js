72124f2a3ea1a9bba0246d6160a8fce5e0d3be94
amitsavani-45 <apsavani4518@gmail.com>
2026-02-05 11:50:06 +0530

    operator

--- operator.js
+++ operator.js
@@ -217,9 +217,15 @@ let a=5;
 let b=6;
 let c= 0 && 2;
 console.log(c);
-*/
+
 
 let a=5;
 let b=6;
 let c=5 || null;
+console.log(c);
+*/
+
+let a=5;
+let b=6;
+let c= true && false || true;
 console.log(c);
