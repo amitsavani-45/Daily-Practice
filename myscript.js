fdab7a29aaf79f4d1e23f624dd004dd95bdebc57
amitsavani-45 <apsavani4518@gmail.com>
2026-02-09 15:47:01 +0530

    myscript

---
+++ myscript.js
@@ -0,0 +1,8 @@
+let b=document.getElementsByTagName("button");
+b[0].addEventListener('click',function f1(){
+    document.body.style.backgroundColor="#333";
+});
+
+b[1].addEventListener('click',function f1(){
+    document.body.style.backgroundColor="#DDD";
+});
