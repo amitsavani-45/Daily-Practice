c311d942d2a469712004721129581e764909b409
amitsavani-45 <apsavani4518@gmail.com>
2026-02-10 17:07:28 +0530

    string

--- string.js
+++ string.js
@@ -26,16 +26,48 @@ console.log("the value is ",res);
 */
 
 
-let s1="mysirg";
-let s2='mysirg';
-let s3=`mysirg`;
-let s4=new String("mysirg");
-// console.log(typeof(s1));
-// console.log(s1==s4);
-// console.log(s1,s2,s3,s4);
+// let s1="mysirg";
+// let s2='mysirg';
+// let s3=`mysirg`;
+// let s4=new String("mysirg");
+// // console.log(typeof(s1));
+// // console.log(s1==s4);
+// // console.log(s1,s2,s3,s4);
 
-console.log(s1.charAt(0));
+// console.log(s1.charAt(0));
 
 
-s1=s1+"Education services";
+// s1=s1+"Education services";
 
+// let s5=`"teacher'sday"`
+// console.log(s5);
+// let a=10;
+// let b=20;
+// let c=a+b;
+// let result=`sum of ${a} and ${b} is ${c}`;
+
+
+let s1="mysirg youtube";
+//console.loh(s1.length);
+
+// console.log(s1.indexOf('e'));
+// console.log(s1.lastIndexOfindexOf('e'));
+// console.log(s1.indexOf('A'));
+// console.log(s1.indexOf('i',7));
+// console.log(s1.includes('sir'));
+// console.log(s1.startsWith('my'));
+// consolele.log(s1.endsWith('ces'));
+
+// console.log(s1.slice(-5,-1));
+// console.log(s1.sunstring(4,10));
+
+
+// s1.replace('s','S');
+//s1.replaceAll('s','S')
+// console.log(s1);
+// console.log(s2);
+
+let s2='private';
+let s3='Limited';
+let s4=s1.concat('',s2,'',s3);
+console.log(s4);
