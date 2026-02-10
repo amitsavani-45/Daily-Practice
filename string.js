c2bfd41ea18c22eb6958b73d9171c565f00245a9
amitsavani-45 <apsavani4518@gmail.com>
2026-02-10 16:14:33 +0530

    strimg

--- string.js
+++ string.js
@@ -1,3 +1,4 @@
+/*
 import promptSync from 'prompt-sync';
 const prompt = promptSync();
 
@@ -22,3 +23,19 @@ let x = prompt("Enter the value of ");
 let y = prompt("Enter the value of ");
 let res=sum(x,y);
 console.log("the value is ",res);
+*/
+
+
+let s1="mysirg";
+let s2='mysirg';
+let s3=`mysirg`;
+let s4=new String("mysirg");
+// console.log(typeof(s1));
+// console.log(s1==s4);
+// console.log(s1,s2,s3,s4);
+
+console.log(s1.charAt(0));
+
+
+s1=s1+"Education services";
+
