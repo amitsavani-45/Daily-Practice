cf6c6c7b8f6e09f499e50acb61ee0f5950eca9a0
amitsavani-45 <apsavani4518@gmail.com>
2026-02-11 11:07:00 +0530

    callback

--- callback.js
+++ callback.js
@@ -36,11 +36,27 @@
 
 
 
+/*
+//Example-1
 //Producer Code
-function someAPI(){
+function someAPI1(){
     let p1=new Promise(function(resolve,reject){
         setTimeout(()=>{
-            console.log("Mein Promise hun");
+            console.log("Mein Promise 1 hun");
+            let x=Math.round(Math.random()*10+1);
+            if(x%2==0)
+                resolve(x);
+            else
+                reject(x);
+        },5000);
+        
+    });
+    return p1;
+}
+function someAPI2(){
+    let p2=new Promise(function(resolve,reject){
+        setTimeout(()=>{
+            console.log("Mein Promise 2 hun");
             let x=Math.round(Math.random()*10+1);
             if(x%2==0)
                 resolve(x);
@@ -49,16 +65,67 @@ function someAPI(){
         },3000);
         
     });
-    return p1;
+    return p2;
 }
 
 //Consumer Code
-let p1=someAPI();
+let p1=someAPI1();
+let p2=someAPI2();
 p1.then((result)=>{
     console.log("Promise fulfilled"+result);
 },(error)=>{
     console.log("Promise Rejected"+error);
 });
-p1.catch(()=>{
-    console.log("In Catch - Rejected ");
+p2.then((result)=>{
+    console.log("Promise fulfilled"+result);
+},(error)=>{
+    console.log("Promise Rejected"+error);
 });
+
+*/
+//Example-2
+//Producer Code
+function someAPI1(){
+    let p1=new Promise(function(resolve,reject){
+        setTimeout(()=>{
+            console.log("Mein Promise 1 hun");
+            let x=Math.round(Math.random()*10+1);
+            if(x%2==0)
+                resolve(x);
+            else
+                reject(x);
+        },5000);
+        
+    });
+    return p1;
+}
+function someAPI2(){
+    let p2=new Promise(function(resolve,reject){
+        setTimeout(()=>{
+            console.log("Mein Promise 2 hun");
+            let x=Math.round(Math.random()*10+1);
+            if(x%2==0)
+                resolve(x);
+            else
+                reject(x);
+        },3000);
+        
+    });
+    return p2;
+}
+
+//Consumer Code
+someAPI1()
+    .then((result)=>{
+            console.log("Promise fulfilled"+result);
+            return someAPI2();
+        },(error)=>{
+            console.log("Promise Rejected"+error);
+    })
+    .then((result)=>{
+            console.log("Promise fulfilled"+result);
+        },(error)=>{
+            console.log("Promise Rejected"+error);
+    });
+
+
