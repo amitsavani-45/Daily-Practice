7346dc2e83251a064709e5f486da5739f28d2f8d
amitsavani-45 <apsavani4518@gmail.com>
2026-02-11 09:15:05 +0530

    callback hell

--- callback.js
+++ callback.js
@@ -1,6 +1,35 @@
-setTimeout(()=>{
-    console.log("Arrow function");
-},3000);
-console.log("line 1");
-console.log("line 2");
-console.log("line 3");
+// setTimeout(()=>{
+//     console.log("Arrow function");
+// },3000);
+// console.log("line 1");
+// console.log("line 2");
+// console.log("line 3");
+
+
+// function f1(fun){
+//     fun(5);
+// }
+
+// f1(function(value1){
+//     f2(value1,function(value2){
+//         f3(value2,function(value3){
+//             f4(value3,function(value4){
+//                 console.log("All Done");
+//             });
+//         });
+//     });
+// });
+
+//callback hell in javascript
+// function step1(value){
+//     f2(value1,step2);
+// }
+// function step2(value2){
+//     f3(value2,step3);
+// }
+// function step3(value3){
+//     f4(value3,()=>{
+//         console.log("All Done");
+//     });
+// }
+// f1(step);
